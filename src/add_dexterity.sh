7c3a2da7cd525bd5262fb27f53c50a489ee8a645
David Glick <davidglick@groundwire.org>
2008-12-20 12:30:09 +0000

    dexterity + chameleon requires a dev version of z3c.form
    svn path=/buildouts/plone-coredev/trunk/; revision=23843

--- src/add_dexterity.sh
+++ src/add_dexterity.sh
@@ -6,5 +6,6 @@
 echo 'Adding dexterity externals...'
 svn propget svn:externals . > EXTERNALS.txt
 svn propget svn:externals https://svn.plone.org/svn/plone/plone.dexterity/buildouts/dev/src >> EXTERNALS.txt
+echo 'z3c.form svn://svn.zope.org/repos/main/z3c.form/trunk' >> EXTERNALS.txt
 svn propset svn:externals -F EXTERNALS.txt .
 svn up
