9dbad55e65aeca310850c9051e7c0062340d0ef1
Hanno Schlichting <hanno@hannosch.eu>
2009-04-03 16:39:09 +0000

    svn.zope.org is reachable via http links now, yeah!
    svn path=/buildouts/plone-coredev/trunk/; revision=26258

--- add_dexterity.sh
+++ add_dexterity.sh
@@ -6,6 +6,6 @@
 echo 'Adding dexterity externals...'
 svn propget svn:externals src > EXTERNALS.txt
 svn propget svn:externals https://svn.plone.org/svn/plone/plone.dexterity/buildouts/dev/src >> EXTERNALS.txt
-echo 'z3c.form svn://svn.zope.org/repos/main/z3c.form/trunk' >> EXTERNALS.txt
+echo 'z3c.form http://svn.zope.org/repos/main/z3c.form/trunk' >> EXTERNALS.txt
 svn propset svn:externals -F EXTERNALS.txt src
 svn up
