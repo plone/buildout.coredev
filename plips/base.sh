db65e49a407f8050af50b56358dbdecc78b9ccde
Ross Patterson <me@rpatterson.net>
2009-09-03 02:36:43 +0000

    Various funkload script improvements
    svn path=/buildouts/plone-coredev/branches/4.0/; revision=29535

--- plips/base.sh
+++ plips/base.sh
@@ -1,19 +1,36 @@
-#!/bin/sh
+#!/bin/bash
+set -x
 
-for i in python2.6, python2.5 python 2.4
+for i in python2.6 python2.5 python2.4
 do
+    if [[ ! -d "/opt/collective.coreloadtests/var/funkload/*-$i" ]]
+    then
+	echo "Begin with $i"
 
-    cd /opt/plone-coredev-4.0 &&
-    bin/instance stop &&
-    $i bootstrap.py &&
-    bin/buildout -vN &&
-    bin/instance start &&
-    sleep 30 &&
-    
-    cd /opt/collective.loadtesting &&
-    bin/fl-run-bench -s collective.coreloadtests --label=$i
+	cd /opt/plone-coredev-4.0 &&
+	bin/instance stop
+	rm -f var/filestorage/Data.fs
+	$i bootstrap.py &&
+	bin/buildout -vN &&
+	bin/instance start &&
+	sleep 30 &&
+	
+	cd /opt/collective.loadtesting &&
+	bin/fl-run-bench -s collective.coreloadtests --label=$i
 
+    fi
 done &&
+
+cd /opt/plone-coredev-4.0 &&
+bin/instance stop
+rm -rf .installed.cfg .mr.developer.cfg parts/ develop-eggs/ Data.fs
+python2.4 bootstrap.py &&
+bin/buildout -vN -c plips/funkload/released.cfg &&
+bin/instance start &&
+sleep 30 &&
+
+cd /opt/collective.loadtesting &&
+bin/fl-run-bench -s collective.coreloadtests --label=released
 
 cd /opt/collective.loadtesting &&
 bin/fl-build-label-reports
--- plips/plips.sh
+++ plips/plips.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+set -x
 
 cd /opt/plone-coredev-4.0 &&
 bin/instance stop
+rm -f var/filestorage/Data.fs
 bin/buildout -vN &&
 bin/instance start &&
 sleep 30 &&
@@ -14,16 +16,15 @@ for i in *.cfg
 do
     if [[ ! -d /opt/collective.loadtesting/var/funkload/*-$i ]]
     then
-
 	cd /opt/plone-coredev-4.0 &&
 	bin/instance stop
+	rm -f var/filestorage/Data.fs
 	bin/buildout -vN -c plips/$i &&
 	bin/instance start &&
 	sleep 30 &&
 	
 	cd /opt/collective.loadtesting &&
 	bin/fl-run-bench -s collective.coreloadtests --label=$i
-
     fi 
 done
 
