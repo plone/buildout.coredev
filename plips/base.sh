c3f25903fda8178a23c191b0272bbf1d0f9a70f0
Ross Patterson <me@rpatterson.net>
2009-09-05 00:27:36 +0000

    Log buildout output, better labels, move Plone version comparisons
    svn path=/buildouts/plone-coredev/branches/4.0/; revision=29563

--- plips/base.sh
+++ plips/base.sh
@@ -1,43 +1,27 @@
 #!/bin/bash
 set -x
+export PYTHON_EGG_CACHE=/opt/plone-coredev-4.0/eggs
 
-for i in python2.6 python2.5 python2.4
+for python in python2.6 python2.5 python2.4
 do
-    if [[ -z $(ls -d /opt/collective.loadtesting/var/funkload/*-$i) ]]
+    label=plone4.0-$python
+    if [[ -z $(ls -d /opt/collective.loadtesting/var/funkload/diff_*-$label) ]]
     then
-	echo "Begin with $i"
 
 	cd /opt/plone-coredev-4.0 &&
 	bin/instance stop
-	rm -f var/filestorage/Data.fs
-	$i bootstrap.py &&
-	bin/buildout -vN &&
+	rm -rf .installed.cfg parts/ develop-eggs/ fake-eggs .mr.developer plips/.installed.cfg plips/fake-eggs/ plips/.mr.developer var/filestorage/Data.fs
+	$python bootstrap.py &>/opt/collective.loadtesting/var/funkload/$label.log &&
+	bin/buildout -vN >>/opt/collective.loadtesting/var/funkload/$label.log 2>&1 &&
 	bin/instance start &&
 	sleep 30 &&
 	
 	cd /opt/collective.loadtesting &&
-	bin/fl-run-bench -s collective.coreloadtests --label=$i
+	bin/fl-run-bench -s collective.coreloadtests --label=$label
 
     fi
 done &&
 
-i=3.3-python2.4
-if [[ -z $(ls -d /opt/collective.loadtesting/var/funkload/*-$i) ]]
-then
-    echo "Begin with $i"
-
-    cd /opt/plone-coredev-4.0 &&
-    bin/instance stop
-    rm -rf .installed.cfg .mr.developer.cfg parts/ develop-eggs/ Data.fs
-    python2.4 bootstrap.py &&
-    bin/buildout -vN -c plips/released.cfg &&
-    bin/instance start &&
-    sleep 30 &&
-
-    cd /opt/collective.loadtesting &&
-    bin/fl-run-bench -s collective.coreloadtests --label=$i
-
-fi
-
 cd /opt/collective.loadtesting &&
-bin/fl-build-label-reports
+bin/fl-build-label-reports --x-label='plone4\.0-.*' --y-label='plone4\.0-.*'
+mv var/funkload/index.html base.html
--- plips/versions.sh
+++ plips/versions.sh
@@ -3,26 +3,26 @@ set -x
 export PYTHON_EGG_CACHE=/opt/plone-coredev-4.0/eggs
 
 cd /opt/plone-coredev-4.0/plips &&
-for plip in plip[0-9]*-*.cfg
+for version in plone*.cfg
 do
-    label=${plip:0:${#plip}-4}
+    label=${version:0:${#version}-4}
     if [[ -z $(ls -d /opt/collective.loadtesting/var/funkload/diff_*-$label) ]]
     then
 
 	cd /opt/plone-coredev-4.0 &&
 	bin/instance stop
 	rm -rf .installed.cfg parts/ develop-eggs/ fake-eggs .mr.developer plips/.installed.cfg plips/fake-eggs/ plips/.mr.developer var/filestorage/Data.fs
-	python2.6 bootstrap.py &>/opt/collective.loadtesting/var/funkload/$label.log &&
-	bin/buildout -vN -c plips/$plip >>/opt/collective.loadtesting/var/funkload/$label.log 2>&1 &&
+	python2.4 bootstrap.py &>/opt/collective.loadtesting/var/funkload/$label.log &&
+	bin/buildout -vN -c plips/$version >>/opt/collective.loadtesting/var/funkload/$label.log 2>&1 &&
 	bin/instance start &&
 	sleep 30 &&
-	
+
 	cd /opt/collective.loadtesting &&
 	bin/fl-run-bench -s collective.coreloadtests --label=$label
-    fi 
 
+    fi
 done
 
-cd /opt/collective.loadtesting
-bin/fl-build-label-reports --x-label='plone4\.0-python2\.6\.cfg' --y-label='plip-.*\.cfg'
-mv var/funkload/index.html plips.html
+cd /opt/collective.loadtesting &&
+bin/fl-build-label-reports --x-label='plone4\.0-python2\.6' --y-label='plone.*\.cfg'
+mv var/funkload/index.html versions.html
