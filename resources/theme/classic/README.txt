de71668aeebee597e17de700c26dabe8c8edaec3
Thijs Jonkman <t.jonkman@gmail.com>
2012-11-29 01:04:17 +0100

    Initial commit of classic theme

---
+++ resources/theme/classic/README.txt
@@ -0,0 +1,6 @@
+This is the Classic Theme applied through Diazo.
+
+To apply this theme, in site setup:
+- Install "Diazo theme support" under Add-on
+- Select "(Unstyles)" as Default themes under Themes
+- Enable "Classic Theme (classic)" under Diazo themes
---
+++ resources/theme/classic/config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/++theme++classic/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+preferred_syntax = :sass
