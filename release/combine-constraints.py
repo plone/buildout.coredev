80a2df844aa2910b6c10e27d17625a0214719e40
Maurits van Rees <maurits@vanrees.org>
2022-10-04 17:06:37 +0200

    Remove icalendar pin for unsupported Python 3.7.
    Update constraints scripts to not handle Python 3.7.

--- release/combine-constraints.py
+++ release/combine-constraints.py
@@ -28,27 +28,23 @@ def parse_file(filename):
 
 
 constraints = os.path.join(directory, "constraints.txt")
-constraints37 = os.path.join(directory, "constraints37.txt")
 constraints38 = os.path.join(directory, "constraints38.txt")
 constraints39 = os.path.join(directory, "constraints39.txt")
 constraints310 = os.path.join(directory, "constraints310.txt")
-for filename in (constraints37, constraints38, constraints39, constraints310):
+for filename in (constraints38, constraints39, constraints310):
     if not os.path.exists(filename):
         print(f"ERROR: {filename} does not exist.")
         print(
-            "Run: tox -p auto -e constraints2,constraints37,constraints38,constraints39,constraints310,constraints"
+            "Run: tox -p auto -e constraints2,constraints38,constraints39,constraints310,constraints"
         )
         sys.exit(1)
 
-c37 = parse_file(constraints37)
 c38 = parse_file(constraints38)
 c39 = parse_file(constraints39)
 c310 = parse_file(constraints310)
 
 # Gather them all in one dictionary.
 pins = defaultdict(dict)
-for package, version in c37.items():
-    pins[package][37] = version
 for package, version in c38.items():
     pins[package][38] = version
 for package, version in c39.items():
@@ -59,20 +55,15 @@ for package, version in c310.items():
 # Combine them.
 combi = []
 for package, versions in pins.items():
-    py37_version = versions.pop(37, None)
     py38_version = versions.pop(38, None)
     py39_version = versions.pop(39, None)
     py310_version = versions.pop(310, None)
-    if py37_version == py38_version == py39_version == py310_version:
+    if py38_version == py39_version == py310_version:
         # All versions are the same.
-        combi.append(f"{package}=={py37_version}")
+        combi.append(f"{package}=={py310_version}")
         continue
     # Some versions are different or missing.
     # Start with the lowest Python.
-    if py37_version is not None:
-        combi.append(f'{package}=={py37_version}; python_version == "3.7"')
-    if not (py38_version or py39_version or py310_version):
-        continue
     if py38_version == py39_version == py310_version:
         # All higher Python versions are the same.
         combi.append(f'{package}=={py38_version}; python_version >= "3.8"')
