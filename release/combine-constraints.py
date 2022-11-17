ad0baf7081f7c7866b11440f9f5b98f867ae75d6
Maurits van Rees <maurits@vanrees.org>
2022-11-17 01:33:54 +0100

    release/tox.ini: support 3.11 when gathering constraints.

--- release/combine-constraints.py
+++ release/combine-constraints.py
@@ -31,17 +31,19 @@ constraints = os.path.join(directory, "constraints.txt")
 constraints38 = os.path.join(directory, "constraints38.txt")
 constraints39 = os.path.join(directory, "constraints39.txt")
 constraints310 = os.path.join(directory, "constraints310.txt")
-for filename in (constraints38, constraints39, constraints310):
+constraints311 = os.path.join(directory, "constraints311.txt")
+for filename in (constraints38, constraints39, constraints310, constraints311):
     if not os.path.exists(filename):
         print(f"ERROR: {filename} does not exist.")
         print(
-            "Run: tox -p auto -e constraints2,constraints38,constraints39,constraints310,constraints"
+            "Run: tox -p auto -e constraints38,constraints39,constraints310,constraints311,constraints"
         )
         sys.exit(1)
 
 c38 = parse_file(constraints38)
 c39 = parse_file(constraints39)
 c310 = parse_file(constraints310)
+c311 = parse_file(constraints311)
 
 # Gather them all in one dictionary.
 pins = defaultdict(dict)
@@ -51,6 +53,8 @@ for package, version in c39.items():
     pins[package][39] = version
 for package, version in c310.items():
     pins[package][310] = version
+for package, version in c311.items():
+    pins[package][311] = version
 
 # Combine them.
 combi = []
@@ -58,25 +62,34 @@ for package, versions in pins.items():
     py38_version = versions.pop(38, None)
     py39_version = versions.pop(39, None)
     py310_version = versions.pop(310, None)
-    if py38_version == py39_version == py310_version:
+    py311_version = versions.pop(311, None)
+    if py38_version == py39_version == py310_version == py311_version:
         # All versions are the same.
-        combi.append(f"{package}=={py310_version}")
+        combi.append(f"{package}=={py311_version}")
         continue
     # Some versions are different or missing.
     # Start with the lowest Python.
-    if py38_version == py39_version == py310_version:
-        # All higher Python versions are the same.
-        combi.append(f'{package}=={py38_version}; python_version >= "3.8"')
-        continue
+    # Check if Python 3.8 differs from the rest.
     if py38_version is not None:
         combi.append(f'{package}=={py38_version}; python_version == "3.8"')
-    if py39_version == py310_version and py39_version is not None:
+
+    # Check if Python 3.9 differs from the rest.
+    if py39_version == py310_version == py311_version and py39_version is not None:
         combi.append(f'{package}=={py39_version}; python_version >= "3.9"')
         continue
     if py39_version is not None:
         combi.append(f'{package}=={py39_version}; python_version == "3.9"')
-    if py310_version is not None:
+
+    # Check if Python 3.10 differs from the rest.
+    if py310_version == py311_version and py310_version is not None:
         combi.append(f'{package}=={py310_version}; python_version >= "3.10"')
+        continue
+    if py310_version is not None:
+        combi.append(f'{package}=={py310_version}; python_version == "3.10"')
+
+    # Check if Python 3.11 differs from the rest.
+    if py311_version is not None:
+        combi.append(f'{package}=={py311_version}; python_version >= "3.11"')
 
 output = "\n".join(combi) + "\n"
 # sanity check:
