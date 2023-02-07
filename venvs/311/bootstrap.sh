3c3897b6f73b3a9189ff0fdc8ae9939a09cf9595
Maurits van Rees <maurits@vanrees.org>
2023-02-07 22:34:47 +0100

    Fix venvs/311/bootstrap.sh

--- venvs/311/bootstrap.sh
+++ venvs/311/bootstrap.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 `which python3.11` -m venv .
 cd ../../
-./venvs/311/bin/pip install -r
+./venvs/311/bin/pip install -r requirements.txt
 ./venvs/311/bin/pip install -r requirements-plone.txt
