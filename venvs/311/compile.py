d584748498e50098ee86c512b84c6488c2173c19
Maurits van Rees <maurits@vanrees.org>
2023-02-10 15:45:04 +0100

    compile.py: output project.dependencies to insert into pyproject.toml.

--- venvs/311/compile.py
+++ venvs/311/compile.py
@@ -1,12 +1,27 @@
-# Print constraints that are installed, but not pinned in Zope.
-# Prerequisites:
-# ./bootstrap.sh
-# bin/pip freeze > installed.txt
-# curl -o zope.txt https://zopefoundation.github.io/Zope/releases/5.8/constraints.txt
+"""Print constraints that are installed, but not pinned in Zope.
+
+Prerequisites:
+
+python3.11 -mvenv .
+bin/pip install -U pip setuptools wheel -c https://dist.plone.org/release/6.0.1/constraints.txt
+bin/pip install Plone -c https://dist.plone.org/release/6.0.1/constraints.txt
+bin/pip freeze --exclude-editable > installed.txt
+curl -o zope.txt https://zopefoundation.github.io/Zope/releases/5.8/constraints.txt
+
+Usage:
+
+bin/python compile.py | pbcopy
+
+Edit pyproject.toml and paste the output into project.dependencies.
+TODO: use tomli to directly write it.
+"""
 with open("installed.txt") as myfile:
     installed = myfile.read().splitlines()
 with open("zope.txt") as myfile:
     zope = myfile.read().splitlines()
+dependencies = []
 for package in installed:
     if package not in zope:
-        print(package)
+        compatible = package.replace("==", "~=")
+        dependencies.append(compatible)
+        print(f'    "{compatible}",')
