e7a0b0df7b159efc4b3962f47e4e3ef2c135adfa
Maurits van Rees <maurits@vanrees.org>
2023-07-20 01:00:42 +0200

    release/tox.ini: no longer generate constraints for Python 3.7.
    Also use python2.7 explicitly instead of python2.
    For some reason, neither of those are found locally though.
    So I generated the 2.7 constraints manually.

--- release/combine-constraints.py
+++ release/combine-constraints.py
@@ -28,27 +28,23 @@ def parse_file(filename):
 
 
 constraints = os.path.join(directory, "constraints.txt")
-constraints37 = os.path.join(directory, "constraints37.txt")
 constraints38 = os.path.join(directory, "constraints38.txt")
 constraints2 = os.path.join(directory, "constraints2.txt")
-for filename in (constraints2, constraints37, constraints38):
+for filename in (constraints2, constraints38):
     if not os.path.exists(filename):
         print(f"ERROR: {filename} does not exist.")
         print(
-            "Run: tox -p auto -e constraints2,constraints37,constraints38,constraints"
+            "Run: tox -p auto -e constraints2,constraints38,constraints"
         )
         sys.exit(1)
 
 c2 = parse_file(constraints2)
-c37 = parse_file(constraints37)
 c38 = parse_file(constraints38)
 
 # Gather them all in one dictionary.
 pins = defaultdict(dict)
 for package, version in c2.items():
     pins[package][2] = version
-for package, version in c37.items():
-    pins[package][37] = version
 for package, version in c38.items():
     pins[package][38] = version
 
@@ -56,9 +52,8 @@ for package, version in c38.items():
 combi = []
 for package, versions in pins.items():
     py2_version = versions.pop(2, None)
-    py37_version = versions.pop(37, None)
     py38_version = versions.pop(38, None)
-    if py2_version == py37_version == py38_version:
+    if py2_version == py38_version:
         # All versions are the same.
         combi.append(f"{package}=={py2_version}")
         continue
@@ -66,16 +61,8 @@ for package, versions in pins.items():
     # Start with Python 2.
     if py2_version is not None:
         combi.append(f'{package}=={py2_version}; python_version < "3.0"')
-    if not (py37_version or py38_version):
-        continue
-    if py37_version == py38_version:
-        # All Py3 versions are the same.
-        combi.append(f'{package}=={py37_version}; python_version >= "3.0"')
-        continue
-    if py37_version is not None:
-        combi.append(f'{package}=={py37_version}; python_version == "3.7"')
     if py38_version is not None:
-        combi.append(f'{package}=={py38_version}; python_version == "3.8"')
+        combi.append(f'{package}=={py38_version}; python_version >= "3.0"')
 
 output = "\n".join(combi) + "\n"
 # sanity check:
