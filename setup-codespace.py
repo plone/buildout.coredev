adf3b9fcfe4c4407e27c13bc1b5dc87a1776ad0d
David Glick <david@glicksoftware.com>
2024-11-30 16:43:14 -0300

    set up VHM

---
+++ setup-codespace.py
@@ -0,0 +1,9 @@
+import os
+import transaction
+
+CODESPACE_NAME = os.environ.get("CODESPACE_NAME", None)
+if CODESPACE_NAME:
+    DOMAIN = os.environ["GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN"]
+    hostname = f"{CODESPACE_NAME}-8080.{DOMAIN}"
+    app.virtual_hosting.set_map(f"{hostname}/VirtualHostBase/https/{hostname}:443/VirtualHostRoot")
+    transaction.commit()
