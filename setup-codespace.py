cf92d4daf41e3be4a2ed15aeb89b6cedb9535697
David Glick <david@glicksoftware.com>
2024-11-30 16:54:26 -0300

    fix vhm path

--- setup-codespace.py
+++ setup-codespace.py
@@ -5,5 +5,5 @@ CODESPACE_NAME = os.environ.get("CODESPACE_NAME", None)
 if CODESPACE_NAME:
     DOMAIN = os.environ["GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN"]
     hostname = f"{CODESPACE_NAME}-8080.{DOMAIN}"
-    app.virtual_hosting.set_map(f"{hostname}/VirtualHostBase/https/{hostname}:443/VirtualHostRoot")
+    app.virtual_hosting.set_map(f"localhost:8080/VirtualHostBase/https/{hostname}:443/VirtualHostRoot")
     transaction.commit()
