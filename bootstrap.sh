1c4c2ba551f2fe68711b1cf51b8e9503c9eee2e7
Maurits van Rees <maurits@vanrees.org>
2025-02-10 16:30:07 +0100

    bootstrap.sh: use Python 3.13.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #/bin/sh
-`which python3.11` -m venv .
+# Warning: the venv module of Python 3.13 edits the .gitignore file.
+# We may want to create the venv in a sub directory.
+`which python3.13` -m venv .
 ./bin/pip install -r requirements.txt
 ./bin/buildout $*
 echo "run plone with: ./bin/instance fg"
