b06a60a3e9bc42ff6284b3c1ed7e39c32d4be684
Maurits van Rees <maurits@vanrees.org>
2025-02-11 09:31:53 +0100

    Revert "bootstrap.sh: use Python 3.13."
    This reverts commit 1c4c2ba551f2fe68711b1cf51b8e9503c9eee2e7.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,5 @@
 #/bin/sh
-# Warning: the venv module of Python 3.13 edits the .gitignore file.
-# We may want to create the venv in a sub directory.
-`which python3.13` -m venv .
+`which python3.11` -m venv .
 ./bin/pip install -r requirements.txt
 ./bin/buildout $*
 echo "run plone with: ./bin/instance fg"
